2656b4299090137e77083185b00cc0f976f6f2fa
oyvind-bakke <oyvind.bakke@ntnu.no>
2017-12-06 13:48:45 +0100

    Small corrections to documentation

--- R/fwerapprox.R
+++ R/fwerapprox.R
@@ -1,9 +1,9 @@
 #' The fwerapprox package
 #'
-#' (1) Computes score test statistics for testing whether a large number of
-#' coefficients (typically corresponding to genetic markers) in a GLM are zero in
-#' presence of a smaller number of covariates (typically environmental covariates) not
-#' included in the test, and provides estimates of correlations between the test
+#' (1) Computes score test statistics for testing whether each of a large number of
+#' coefficients (typically corresponding to genetic markers) in a GLM is zero in
+#' presence of a smaller number of covariates (typically environmental covariates),
+#' and provides estimates of correlations between the test
 #' statistics, and (2) computes Glaz–Johnson-type intersection approximations of the
 #' asymptotic multivariate normal distribution of the test statistics. See Halle et al.,
 #' "Efficient and powerful familywise error control in genome-wide association studies
